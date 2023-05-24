f76731defad701da0d77ff0006688de282d849b9
Ganaa Li <gangzhaorige.li@gmail.com>
2023-05-23 20:08:29 -0700

    Add flash-cards springboot server

---
+++ server/src/test/java/com/flashcards/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcards.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
