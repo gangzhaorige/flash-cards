4b5fb1457fbee3f273707e1efac2bf99212a1172
Ganaa Li <gangzhaorige.li@gmail.com>
2023-05-28 02:07:31 -0700

    refactor user to have roles

---
+++ server/src/main/java/com/flashcards/server/entity/Role.java
@@ -0,0 +1,44 @@
+package com.flashcards.server.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 20)
+    @Enumerated(EnumType.STRING)
+    private ERole name;
+
+    public Role() {
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
+}
--- server/src/main/java/com/flashcards/server/entity/User.java
+++ server/src/main/java/com/flashcards/server/entity/User.java
@@ -1,15 +1,20 @@
 package com.flashcards.server.entity;
 
 import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinColumn;
 @Entity
 public class User {
     @Id
@@ -24,6 +29,13 @@ public class User {
 
     @Column(nullable = false, length = 255)
     private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @Column(nullable = false, unique = true)
+    private String username;
 
     @JsonFormat(pattern="yyyy-MM-dd")
     Date dateCreated;
