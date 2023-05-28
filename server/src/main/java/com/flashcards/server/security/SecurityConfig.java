aaef1c7bb194f6e5b8697f31ea56616bbdcedd61
Ganaa Li <gangzhaorige.li@gmail.com>
2023-05-28 02:41:10 -0700

    add jwt authority

--- server/src/main/java/com/flashcards/server/security/SecurityConfig.java
+++ server/src/main/java/com/flashcards/server/security/SecurityConfig.java
@@ -1,21 +1,47 @@
 package com.flashcards.server.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.flashcards.server.security.jwt.AuthEntryPointJwt;
+import com.flashcards.server.security.jwt.AuthTokenFilter;
+import com.flashcards.server.security.services.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	@Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
 
     @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+ 
+    @Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> 
+		http
+		.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+		.authorizeHttpRequests((requests) -> 
             requests.requestMatchers("/auth/**").permitAll()
             .anyRequest().authenticated()
 		);
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
     
