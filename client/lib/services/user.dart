277ec9c360a2618590145ec3e294c39be792590d
Ganaa Li <gangzhaorige.li@gmail.com>
2023-06-03 15:54:31 -0700

    added user for auth

---
+++ client/lib/services/user.dart
@@ -0,0 +1,12 @@
+class User {
+
+  final String username;
+  final String email;
+  final int id;
+  
+  const User({
+    required this.username,
+    required this.email,
+    required this.id,
+  });
+}
