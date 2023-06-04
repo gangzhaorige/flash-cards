abf46fec3168c7ad6e8536941046b25a2ce693d1
Ganaa Li <gangzhaorige.li@gmail.com>
2023-06-04 00:48:45 -0700

    added basic signup page and functionality

---
+++ client/lib/models/signup_model.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:client/routes/navigation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../services/dio_request.dart';
+
+class RegisterModel extends ChangeNotifier {
+
+  String _username = '';
+
+  String _password = '';
+
+  String _email = '';
+
+  String get username => _username;
+
+  String get password => _password;
+
+  String get email => _email;
+
+  void updateUsername(String newUsername) {
+    _username = newUsername;
+    notifyListeners();
+  }
+
+  void updatePassword(String newPassword) {
+    _password = newPassword;
+    notifyListeners();
+  }
+
+  void updateEmail(String newEmail) {
+    _email = newEmail;
+    notifyListeners();
+  }
+
+  Future<void> signup() async {
+    var response = await DioApi.postRequest(
+      path: '/auth/signup',
+      data: jsonEncode({'email' : _email,'username' : _username, 'password' : _password}),
+    );
+    print(response.data);
+    if(response.statusCode == 201) {
+      Get.to(Routes.login);
+    }
+  }
+}
---
+++ client/lib/views/register_view.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/signup_model.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+
+  late TextEditingController _emailController;
+  late TextEditingController _usernameController;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameController = TextEditingController();
+    _passwordController = TextEditingController();
+    _emailController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    _emailController.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            const Text('Sign Up!'),
+            Selector<RegisterModel, Function>(
+              selector: (_, registerProvider) => registerProvider.updateEmail,
+              builder: (_, updateEmail, __) {
+                return TextField(
+                  controller: _emailController,
+                  onChanged:(value) {
+                    updateEmail(value);
+                  },
+                );
+              }
+            ),
+            Selector<RegisterModel, Function>(
+              selector: (_, registerProvider) => registerProvider.updateUsername,
+              builder: (_, updateUsername, __) {
+                return TextField(
+                  controller: _usernameController,
+                  onChanged: (value) {
+                    updateUsername(value);
+                  },
+                );
+              }
+            ),
+            Selector<RegisterModel, Function>(
+              selector: (_, registerProvider) => registerProvider.updatePassword,
+              builder: (_, updatePassword, __) {
+                return TextField(
+                  controller: _passwordController,
+                  onChanged:(value) {
+                    updatePassword(value);
+                  },
+                );
+              }
+            ),
+            MaterialButton(
+              onPressed:() async {
+                await context.read<RegisterModel>().signup();
+              },
+              color: Colors.blue,
+              child: const Text('REGISTER ACCOUNT'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
