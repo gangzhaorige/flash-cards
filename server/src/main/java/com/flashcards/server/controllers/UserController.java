ce7ba315304ce4baec463828daa445613ef7d689
Ganaa Li <gangzhaorige.li@gmail.com>
2023-06-04 01:52:26 -0700

    refactor to make response json based

--- server/src/main/java/com/flashcards/server/controllers/UserController.java
+++ server/src/main/java/com/flashcards/server/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.flashcards.server.controllers;
 
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +19,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.flashcards.server.entity.Card;
 import com.flashcards.server.entity.Role;
 import com.flashcards.server.entity.User;
 import com.flashcards.server.payload.requests.CardInfoRequest;
-import com.flashcards.server.payload.responses.CardInfoResponse;
 import com.flashcards.server.payload.responses.MessageResponse;
 import com.flashcards.server.payload.responses.UserInfoResponse;
 import com.flashcards.server.repositories.CardRepository;
@@ -37,6 +41,7 @@ public class UserController {
 
     @GetMapping("")
     public ResponseEntity<?> getAllUsers() {
+        Map<String, Object> json = new HashMap<String, Object>();
         Iterable<User> users = userRepository.findAll();
         List<UserInfoResponse> list = new ArrayList<>();
         for(User user : users) {
@@ -47,33 +52,44 @@ public class UserController {
             }
             list.add(new UserInfoResponse(user.getId(), user.getEmail(), user.getUsername(), lStrings));
         }
-        return ResponseEntity.ok().body(list);
+        json.put("users", list);
+        return ResponseEntity.ok().body(json);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getUser(@PathVariable Long id) {
-        if(userRepository.existsById(id)) {
-            Optional<User> user = userRepository.findById(id);
-            User userInfo = user.get();
-            Set<Role> set = userInfo.getRoles();
+        Map<String, Object> json = new HashMap<String, Object>();
+        Optional<User> result = userRepository.findById(id);
+        if(result.isPresent()) {
+            User user = result.get();
+            Set<Role> set = user.getRoles();
             List<String> lStrings = new ArrayList<>();
             for(Role role : set) {
                 lStrings.add(role.getName().toString());
             }
-            return ResponseEntity.ok().body(new UserInfoResponse(userInfo.getId(), userInfo.getEmail(), userInfo.getUsername(), lStrings));
+            UserInfoResponse userInfo = new UserInfoResponse(user.getId(), user.getEmail(), user.getUsername(), lStrings);
+            json.put("user", userInfo);
+            return ResponseEntity.ok().body(json);
         }
-        return ResponseEntity.ok().body(new MessageResponse("ID not found!"));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new MessageResponse("The user does not exist."));
     }
 
     @PostMapping("/{id}/cards")
     public ResponseEntity<?> createCardForUser(@PathVariable Long id, @Valid @RequestBody CardInfoRequest cardInfo) {
-        if(userRepository.existsById(id)) {
+        Optional<User> userResult = userRepository.findById(id);
+        if(userResult.isPresent()) {
             Card card = new Card(cardInfo.getQuestion(), cardInfo.getIsMultipleChoice(), cardInfo.getAnswer());
-            User user = userRepository.findById(id).get();
+            User user = userResult.get();
             card.setUser(user);
             cardRepository.save(card);
-            return ResponseEntity.ok().body(new CardInfoResponse(card.getId(), card.getQuestion(), card.getIsMultipleChoice(), card.getAnswer()));
+            URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/cards/{id}").buildAndExpand(card.getId()).toUri();
+            return ResponseEntity.created(uri).build();
         }
-        return ResponseEntity.ok().body(new MessageResponse("ID not found!"));
+        Map<String, Object> json = new HashMap<String, Object>();
+        json.put("message", "Validation errors in your request.");
+        List<String> errors = new ArrayList<>();
+        errors.add("User id does not exist.");
+        json.put("errors", errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(json);
     }
 }
